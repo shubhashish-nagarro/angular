9c24dbe5c2d8107d1153ad180d6e2f2b7bcd03c7
Shubhashish Das <shubhashish.das@nagarro.com>
2026-02-11 10:42:09 +0530

    Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, linkedSignal, Signal, signal, untracked } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,42 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Angular20');
+  title = signal('Angular');
+  clickCount = signal(0);
+  latestTitle = computed(() => "Hello, " + this.title() + ' ' + this.clickCount());
+  showCount = signal(false);
+  conditionalCount = computed(() => {
+    if (this.showCount()) {
+      return `The count is ${this.clickCount()}.`;
+    } else {
+      return 'Nothing to see here!';
+    }
+  });
+  // 
+  shippingOptions: Signal<string[]> = this.getFruitsArray();
+  // Initialize selectedOption to the first shipping option.
+  selectedOption = linkedSignal(() => this.shippingOptions()[0]);
+
+  constructor() {
+    effect(() => {
+      console.log(`show value is ${this.showCount()} and the counter is ${untracked(this.clickCount)}`);
+    });
+  }
+
+  increaseCount() {
+    this.clickCount.update((value) => value + 1);
+    this.selectedOption.set(this.shippingOptions()[this.clickCount()]);
+  }
+
+  show() {
+    this.showCount.set(!this.showCount());
+  }
+
+  getFruitsArray() {
+    return signal(['Apple', 'Banana', 'Mango', 'Orange']);
+  }
+
+  getVegArray() {
+    return signal(['Potato', 'Onion']);
+  }
 }
